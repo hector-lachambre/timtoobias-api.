78a746eb085e3f691d8fb8c197af05c71b4aef49
hector-lachambre <hectorlachambre.pro@gmail.com>
2019-03-13 22:53:18 +0100

    DEV: Add cors header

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ func main() {
 func provideDatas(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	cache := &Cache{}
 
