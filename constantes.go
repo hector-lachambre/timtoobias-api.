261ab7b9b3654f745591cb5d6fc4ba2375560f0e
hector-lachambre <hectorlachambre.pro@gmail.com>
2019-03-17 19:08:45 +0100

    DEV: Mise à jour des constantes pour le changement de proriétaire de la chaine.

--- constantes.go
+++ constantes.go
@@ -1,7 +1,7 @@
 package main
 
-const YT_HuzId_main = "UCwwTPrN3XUFEGzCb6XuVniQ"
-const YT_HuzId_second = "UClTaTsOdHo6UNSCYAPN1YNQ"
+const YT_HuzId_main = "UClTaTsOdHo6UNSCYAPN1YNQ"
+const YT_HuzId_second = "UC0pREpRaQjcC71d_4Db40iQ"
 const YT_key = "AIzaSyBW51eml_88bbXU6tdm1JIfQONMQy-2eqs"
 const Twitch_HuzId = "42428057"
 const Twitch_key = "7m3kxyorkss6bg4fzgz1pyueuhcjyb"
