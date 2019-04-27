4edc10ed59668f6bf3163bce68ab37fb69e7d369
hector-lachambre <hectorlachambre.pro@gmail.com>
2019-04-27 20:05:40 +0200

    Correction of channel ids

--- constantes.go
+++ constantes.go
@@ -1,5 +1,5 @@
 package main
 
-const YT_HuzId_main = "UCwwTPrN3XUFEGzCb6XuVniQ"
-const YT_HuzId_second = "UClTaTsOdHo6UNSCYAPN1YNQ"
+const YT_HuzId_main = "UClTaTsOdHo6UNSCYAPN1YNQ"
+const YT_HuzId_second = "UC0pREpRaQjcC71d_4Db40iQ"
 const Twitch_HuzId = "42428057"
